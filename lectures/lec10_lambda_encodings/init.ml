7160050ad9e316eff0e8ac1d81a3f74123c45049
KC Sivaramakrishnan <kc@kcsrk.info>
2025-09-05 21:52:29 +0530

    updates

--- lectures/lec10_lambda_encodings/init.ml
+++ lectures/lec10_lambda_encodings/init.ml
@@ -1,6 +1,6 @@
-#directory "/lectures/lambda";;
-#load_rec "/lectures/lambda/syntax.cmo";;
-#load_rec "/lectures/lambda/lambda_parse.cmo";;
-#load_rec "/lectures/lambda/eval.cmo";;
+#directory "/cs3100_iitm/lambda";;
+#load_rec "/cs3100_iitm/lambda/syntax.cmo";;
+#load_rec "/cs3100_iitm/lambda/lambda_parse.cmo";;
+#load_rec "/cs3100_iitm/lambda/eval.cmo";;
 
 open Syntax
