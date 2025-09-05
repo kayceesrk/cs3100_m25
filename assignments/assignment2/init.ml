3f530d5e956b8e8c3c1355f84a1951977253347c
KC Sivaramakrishnan <kc@kcsrk.info>
2025-09-05 22:05:12 +0530

    Release assignment2

---
+++ assignments/assignment2/init.ml
@@ -0,0 +1,6 @@
+#directory "/cs3100_iitm/lambda";;
+#load_rec "/cs3100_iitm/lambda/syntax.cmo";;
+#load_rec "/cs3100_iitm//lambda/lambda_parse.cmo";;
+#load_rec "/cs3100_iitm//lambda/alpha.cmo";;
+
+let alpha_equiv = Alpha.alpha_equiv
